0ff22ae7981e190c36b90561ca0a8077d6cd8311
GustavoCaso <gustavo@airrefund.com>
2016-02-17 18:57:54 +0100

    Start working on parse math expression

--- chapter10.rb
+++ chapter10.rb
@@ -1,3 +1,22 @@
+class Stack
+  def initialize
+    @store = Array.new
+  end
+
+  def pop
+    @store.pop
+  end
+
+  def push(element)
+    @store.push(element)
+    self
+  end
+
+  def size
+    @store.size
+  end
+end
+
 class BinaryTree
   attr_accessor :data, :left_child, :right_child
   def initialize(data)
@@ -34,4 +53,34 @@ class BinaryTree
     right_child_display = right_child ? right_child.display_tree : []
     [data, left_child_display, right_child_display]
   end
+end
+
+# Build a function to parse mathematical expressions
+
+def build_parse_tree(exp)
+  exp_arr = exp.split
+  tree_stack = Stack.new
+  tree = BinaryTree.new('')
+  current_tree = tree
+  exp_arr.each do |i|
+    if i == '('
+      current_tree.insert_left('')
+      tree_stack.push(current_tree)
+      current_tree = current_tree.left_child
+    elsif i.match(/\d/)
+      current_tree.set_root_value(Integer(i))
+      parent = tree_stack.pop
+      current_tree = parent
+    elsif ['+', '-', '*', '/'].include?(i)
+      current_tree.set_root_value(i)
+      current_tree.insert_right('')
+      tree_stack.push(current_tree)
+      current_tree = current_tree.right_child
+    elsif i == ')'
+      current_tree = tree_stack.pop
+    else
+      fail 'Invalid Argument'
+    end
+  end
+  tree
 end
